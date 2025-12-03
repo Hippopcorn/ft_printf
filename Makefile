2af3b0a5cdb9052d2bd9c158914014cbd17f04a9
Elsa Varache <evarache@z2r12p4.42lyon.fr>
2025-12-03 15:42:55 +0100

    first function

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: evarache <evarache@student.42lyon.fr>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/11/13 11:40:40 by evarache          #+#    #+#              #
-#    Updated: 2025/12/02 16:28:15 by evarache         ###   ########.fr        #
+#    Updated: 2025/12/03 13:32:06 by evarache         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,25 +14,33 @@ CC = cc
 CFLAGS = -Wall -Wextra -Werror
 SOURCES = ft_printf.c
 OBJECTS = $(SOURCES:.c=.o)
-NAME = libftprintf
+NAME = libftprintf.a
 
-all: $(NAME)
+LIBFT_DIR = libft
+LIBFT = $(LIBFT_DIR)/libft.a
+
+# Compile la libft via SON Makefile
+$(LIBFT):
+	$(MAKE) -C $(LIBFT_DIR)
+all: $(NAME) $(LIBFT)
+
 
 $(NAME): $(OBJECTS)
-	$(CC) -o $@ $^
+	cp $(LIBFT) $(NAME)
+	ar rcs $(NAME) $(OBJECTS)
 
 %.o: %.c
 	$(CC) $(CFLAGS) -c $< -o $@
 
 clean:
 	rm -f $(OBJECTS)
-
+	$(MAKE) -C $(LIBFT_DIR) clean
+	
 fclean: clean
 	rm -f $(NAME)
-
+	$(MAKE) -C $(LIBFT_DIR) fclean
+	
 re: fclean all
 
-test: re
-	./$(NAME)
 
 .PHONY: all clean fclean re test
--- ft_printf.c
+++ ft_printf.c
@@ -1,1 +1,81 @@
-int ft_printf(const char *, ...);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evarache <evarache@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/03 13:42:04 by evarache          #+#    #+#             */
+/*   Updated: 2025/12/03 15:18:54 by evarache         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int	count_carac(char *str, char c)
+{
+	int i;
+	int count;
+	
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+char	type_conversion(char *str, int index)
+{
+	char	type_conv;
+	
+	while(str[index])
+	{
+		if (str[index] == '%')
+		{
+			type_conv = str[index + 1];
+			break ;
+		}
+		index ++;
+	}
+	if (type_conv != 'c' && type_conv != 's' && type_conv != 'p' &&
+		type_conv != 'd' && type_conv != 'i' && type_conv != 'u' && 
+		type_conv != 'x' && type_conv != 'X' && type_conv != '%')
+		return (NULL);
+	return (type_conv);
+	
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list args;
+	int 	i;
+	int 	n_arg;
+
+	i = 0;
+	n_arg = 0;
+	va_start(args, format);
+	
+	
+	// pour parcourir les arguments
+	while (i < count_carac((char *)format, '%'))  // on parcoure le nombres d'arguments
+	{
+        printf("%c ", va_arg(args, int));  // avance de 1 arg a chaque appel
+		i++;
+	}
+	va_end(args);
+	return (0);
+}
+
+
+
+int main()
+{
+	//printf("%d\n", count_carac("hfdgf%khafk%kaba%%kb%", '%'));
+	ft_printf("c%ou%co%u", 'c', 'u', 'i');
+	return (0);
+}
